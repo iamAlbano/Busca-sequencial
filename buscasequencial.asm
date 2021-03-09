3f643981db15c5314336809b1e5f5391a7f4a210
KassioRF <kassio_rf@hotmail.com>
2021-03-09 17:58:40 -0300

    Algoritmo busca sequencial

--- buscasequencial.asm
+++ buscasequencial.asm
@@ -1,7 +1,11 @@
 .data
-	array1: .space 40 #declara um array de 10 posições
+	array1: .space 40 	#declara um array de 10 posições
 	msg: .asciiz "\t ---- Insira os valores do vetor ----- \n\n"
+	msg_busca: .asciiz "\t --- Insira o valor a ser buscado no vetor: --- \n\n"
 	msg_b: .asciiz " valor: "
+	msg_encontrado: .asciiz " Valor encontrado: "
+	msg_naoEncontrado: .asciiz "Valor não encontrado... "
+	
 .text
 	
 	addi $s0, $zero, 0	 #endereço base do vetor
@@ -38,13 +42,50 @@
 		j while
 		
 	exit:
-		
+	
 	
 		
+	# Inserir o valor a ser buscado
+	li $v0, 4 	# Mensagem para usuário
+	la $a0, msg_busca
+	syscall	
+	
+	li $v0, 5 #ler valor inserido pelo usuario
+	syscall
+	move $s4, $v0
+	
+	# algoritmo de busca sequencial.
+	li $t0, 1
+	move $s0, $zero
+	
+	while2:
+		bgt $t0, $t1, exit2
+		lw $s5, array1($s0)
 		
+		beq $s4,$s5 encontrado	#compara o valor inserido com o valor buscado
 		
+		addi $s0, $s0, 4 	#aponta para a proxima posição do vetor
+		addi $t0, $t0, 1 	# $t++
+	
+		j while2
+			
+
+	encontrado:
+		li $v0, 4
+		la $a0, msg_encontrado		
+		syscall
 		
-		#li $v0, 5 # Scan inteiro
+		li $v0, 10
+		syscall
+	
+	exit2:	
+		li $v0, 4
+		la $a0, msg_naoEncontrado		
+		syscall
+	 	
+	 	#"retorno para o usuario"	
+	 
+		
 		
 	
 	
